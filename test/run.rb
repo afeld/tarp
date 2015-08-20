16526633a5eae4618b38a4fe3f639a3964a2f749
Aidan Feldman <aidan.feldman@gmail.com>
2015-08-20 05:19:07 -0400

    make the tracer a module

--- test/run.rb
+++ test/run.rb
@@ -2,6 +2,8 @@ require 'pry-byebug'
 require_relative '../tracer'
 require_relative '../src'
 
+Tracer.enable
+
 MyClass.public_class_method
 my_instance = MyClass.new
 my_instance.public_instance_method
