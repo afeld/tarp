92ff07483812dad43d8078cc8d89cf6a4f1ac1d9
Aidan Feldman <aidan.feldman@gsa.gov>
2015-09-26 20:50:07 -0500

    allow the logger to accept objects

--- lib/tarp.rb
+++ lib/tarp.rb
@@ -1,8 +1,18 @@
 require_relative 'tarp/version'
 require_relative 'tarp/tracer'
+require_relative 'tarp/trace_method'
 
 module Tarp
+  # TODO allow a block to be passed so it doesn't need to be evaluated if not in debug mode
   def self.log(msg)
-    puts "DEBUG: #{msg}" if ENV['DEBUG']
+    if ENV['DEBUG']
+      require 'pp'
+      if msg.is_a?(String)
+        puts "DEBUG: #{msg}"
+      else
+        puts "DEBUG:"
+        pp msg
+      end
+    end
   end
 end
